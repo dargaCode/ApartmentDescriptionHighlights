b23bdbecd214b4a571eb0cf1de7b7902719f6de6
dargaCode <darga.code@gmail.com>
2016-09-14 16:47:39 -0700

    Refactor phrase splitting code - Extract function splitStringIntoPhrases - Extract constant PHRASE_ENDING_CHARACTERS   - Array is joined and inserted into regex, for easier upkeep

--- snippet/snippet.js
+++ snippet/snippet.js
@@ -1,20 +1,41 @@
 
 'use strict'; // enable 'let' declarations
 
-const str = 'To be, or not to be, that is the question. Whether tis nobler! in the mind, to suffer; the slings and arrows! of outrageous fortune? or to take arms - against a sea of troubles, and by opposing? end them:';
-console.log(`string: '${str}' \n`);
+// CONSTANTS
 
-// match phrases ending in commas or periods
-const phraseRegex = new RegExp('.+?[-,.:;!?]','g');
-const phrases = [];
-let matchInfo = [];
+// data-driven for ease of tuning
+const PHRASE_ENDING_CHARACTERS = [
+  '.',
+  '!',
+  '?',
+  ',',
+  ';',
+  ':',
+  '-',
+];
 
-// find all regex matches, and save them
-while ((matchInfo = phraseRegex.exec(str)) !== null) {
-  const match = matchInfo[0].trim();
-  console.log(`match: '${match}'`);
-  phrases.push(match);
+// FUNCTIONS
+
+function splitStringIntoPhrases(description) {
+  const endingChars = PHRASE_ENDING_CHARACTERS.join('');
+  // match substrings ending in one of the phrase ending chars
+  const phraseRegex = new RegExp(`.+?[${endingChars}]`,'g');
+  const results = [];
+
+  let matchInfo = [];
+  // find all regex matches, and save them
+  while ((matchInfo = phraseRegex.exec(str)) !== null) {
+    const match = matchInfo[0].trim();
+    results.push(match);
+  }
+
+  return results;
 }
 
-console.log('');
+// MAIN
+
+const str = 'To be, or not to be, that is the question. Whether tis nobler! in the mind, to suffer; the slings and arrows! of outrageous fortune? or to take arms - against a sea of troubles, and by opposing? end them:';
+const phrases = splitStringIntoPhrases(str);
+
+console.log(`string: '${str}' \n`);
 console.log("All matching phrases: ", phrases);
