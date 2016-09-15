3a68470678737be8740b30c5c6f7d331dc6560e5
dargaCode <darga.code@gmail.com>
2016-09-15 15:43:58 -0700

    Add more tuning data to contants.js - Make phraseEndingChars better match this assignment   - Remove '-' and ',' - Add array of keywords objects for matching against query

--- snippet/constants.js
+++ snippet/constants.js
@@ -4,10 +4,34 @@ const CONSTANTS = {
     '.',
     '!',
     '?',
-    ',',
     ';',
     ':',
-    '-',
+  ],
+  keywords: [
+    {
+      name: 'balcony',
+      categories: ['light', 'nature', 'view'],
+    },
+    {
+      name: 'courtyard',
+      categories: ['amenities', 'light', 'nature', 'privacy'],
+    },
+    {
+      name: 'gym',
+      categories: ['amenities', 'fitness', 'health'],
+    },
+    {
+      name: 'stove',
+      categories: ['appliance', 'kitchen', 'modern'],
+    },
+    {
+      name: 'trails',
+      categories: ['amenities', 'health', 'nature'],
+    },
+    {
+      name: 'windows',
+      categories: ['light', 'nature', 'view'],
+    },
   ],
 }
 
