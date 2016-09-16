570a6d72e2992f004d0720657a0cca55aff124f9
dargaCode <darga.code@gmail.com>
2016-09-16 00:54:49 -0700

    Clean up app.js - Move constants into new // CONSTANTS section - Update demo document/query to be different from any test cases

--- app.js
+++ app.js
@@ -6,12 +6,14 @@
 const DESCRIPTIONS = require('./constants/example-descriptions');
 const SNIPPET = require('./snippet/snippet');
 
+// CONSTANTS
+
+const description = DESCRIPTIONS.marc;
+const query = 'urban view';
+
 // MAIN
 
-console.log('\n\n\nRUNNING APP.JS "MAIN" \n')
-
-const description = DESCRIPTIONS.matt;
-const query = '#@$%^AMenitiES#&* $%#^&*NATURE!!@#$ ?kitcHEN?%{}';
+console.log('\n\n\nRUNNING APP.JS \n')
 
 const snippet = SNIPPET.generateSnippet(description, query);
 
